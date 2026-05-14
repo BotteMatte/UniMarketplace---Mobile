241ecf51095b360e9790a7027feed46e6c78e2c2
Matteo Botteghi <matteo.botteghi6@studio.unibo.it>
2026-05-14 11:43:07 +0200

    reso automatico tema chiro/scuro
    se il telefono é in modalita scura l app si apre direttamente in modalita scura

--- app/src/main/java/com/example/unimarketplace/MainActivity.kt
+++ app/src/main/java/com/example/unimarketplace/MainActivity.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -27,7 +27,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            var isDarkTheme by remember { mutableStateOf(false) }
+            val systemInDarkTheme = isSystemInDarkTheme()
+            var isDarkTheme by remember(systemInDarkTheme) { mutableStateOf(systemInDarkTheme) }
 
             UniMarketplaceTheme(darkTheme = isDarkTheme) {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
