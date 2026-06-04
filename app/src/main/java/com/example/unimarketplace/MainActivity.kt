427d559d449caa0e0f49fcd0b449e5adf8c78626
Matteo Botteghi <matteo.botteghi6@studio.unibo.it>
2026-06-04 11:16:44 +0200

    rimozione librerire inutilizzate

--- app/src/main/java/com/example/unimarketplace/MainActivity.kt
+++ app/src/main/java/com/example/unimarketplace/MainActivity.kt
@@ -10,15 +10,12 @@ import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.unimarketplace.ui.navigation.AppNavigation
 import com.example.unimarketplace.ui.theme.UniMarketplaceTheme
 
